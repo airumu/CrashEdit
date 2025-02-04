8a22a37de74438bb7a666de4822b5c789c007ec7
Airumu <aairumu@gmail.com>
2025-02-04 22:06:36 +0900

    update HelpWindow

--- CrashEdit/Forms/HelpWindow.Designer.cs
+++ CrashEdit/Forms/HelpWindow.Designer.cs
@@ -43,7 +43,7 @@ namespace CrashEdit.CE
             fraHexViewer.Controls.Add(lblHexViewer);
             fraHexViewer.Location = new Point(4, 3);
             fraHexViewer.Name = "fraHexViewer";
-            fraHexViewer.Size = new Size(258, 248);
+            fraHexViewer.Size = new Size(253, 248);
             fraHexViewer.TabIndex = 15;
             fraHexViewer.TabStop = false;
             fraHexViewer.Text = "Hex Viewer";
@@ -54,7 +54,7 @@ namespace CrashEdit.CE
             lblHexViewer.ForeColor = SystemColors.ControlText;
             lblHexViewer.Location = new Point(6, 19);
             lblHexViewer.Name = "lblHexViewer";
-            lblHexViewer.Size = new Size(246, 210);
+            lblHexViewer.Size = new Size(241, 210);
             lblHexViewer.TabIndex = 0;
             lblHexViewer.Text = resources.GetString("lblHexViewer.Text");
             // 
