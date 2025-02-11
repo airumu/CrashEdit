c06239d913b9bb5755ede9f439b4c6de030af169
Airumu <aairumu@gmail.com>
2025-02-11 14:39:34 +0900

    fix Menu_Export_Linked_VAB_DLS() dialog

--- CrashEdit/Controllers/Music/OldMusicEntryController.cs
+++ CrashEdit/Controllers/Music/OldMusicEntryController.cs
@@ -57,7 +57,7 @@ namespace CrashEdit.CE
 
         private void Menu_Export_Linked_VAB_DLS()
         {
-            if (DarkMessageBox.ShowWarning("Exporting to DLS is experimental.\n\nContinue anyway?", "Export Linked VAB as DLS", DarkDialogButton.YesNo) != DialogResult.Yes)
+            if (DarkMessageBox.ShowWarning("Exporting to DLS is experimental.\nContinue anyway?", "Export Linked VAB as DLS", DarkDialogButton.YesNo) != DialogResult.Yes)
             {
                 return;
             }
