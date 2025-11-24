9abb9edc3863395c05d0713b42970823867ba6be
Airumu <aairumu@gmail.com>
2025-11-24 19:39:27 +0900

    Add T21 editor

--- Crash/Formats/Crash Formats/T21/T21Entry.cs
+++ Crash/Formats/Crash Formats/T21/T21Entry.cs
@@ -1,14 +1,27 @@
 namespace CrashEdit.Crash
 {
-    public sealed class T21Entry : MysteryMultiItemEntry
+    public sealed class T21Entry : Entry
     {
-        public T21Entry(IEnumerable<byte[]> items, int eid) : base(items, eid)
+        public T21Entry(IEnumerable<byte[]> items, int eid) : base(eid)
         {
+            Items.AddRange(items);
         }
 
         public override string Title => $"T21 ({EName})";
         public override string ImageKey => "Painting";
 
         public override int Type => 21;
+
+        public List<byte[]> Items { get; } = [];
+
+        public override UnprocessedEntry Unprocess()
+        {
+            byte[][] items = new byte[Items.Count][];
+            for (int i = 0; i < Items.Count; i++)
+            {
+                items[i] = Items[i];
+            }
+            return new UnprocessedEntry(items, EID, Type);
+        }
     }
 }
